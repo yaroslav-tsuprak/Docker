e59cd7cb2296dc07817293124aef2030f2109475
yaroslav-tsuprak <yaroslav.tsuprak@gmail.com>
2020-10-05 23:21:10 +0700

    Added Mongo config

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const {connectDb} = require("./helpers/db");
-const {host, port} = require("./configuration");
+const {host, port, db} = require("./configuration");
 const app = express();
 
-const sratServer = () => {
+const starServer = () => {
 	app.listen(port, () => {
 		console.log(`Starting api service on port: ${port}`);
 		console.log(`On host: ${host}`);
+		console.log(`Our database: ${db}`);
 	});
-}
+};
 
 app.get("/test", (req, res) => {
 	res.send("Our api service is working correctly");
 })
 
-connectDb
+connectDb()
 .on("error", console.log)
 .on("disconnected", connectDb)
 .once("open", startServer);
